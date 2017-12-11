15b9eb459e942f8bbe589e37a93a2a1c8abfdd06
SAMI BIRNBAUM <sami_birnbaum@btconnect.com>
2017-12-11 11:49:13 +0000

    A12 Complete

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -14,4 +14,11 @@ RSpec.describe WelcomeController, type: :controller do
           expect(response).to render_template("about")
         end
     end 
+
+    describe "GET faq" do #controller serving up the faq view off the back of an http request
+        it "renders the faq view" do
+            get :faq        # call the faq method (action) in our controller
+            expect(response).to render_template("faq")
+        end
+    end
 end
