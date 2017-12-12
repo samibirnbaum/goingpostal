6f6ea5830aa207394ae9b9978daeeb1c8eec5f18
SAMI BIRNBAUM <sami_birnbaum@btconnect.com>
2017-12-12 15:58:36 +0000

    C14 Complete

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :post    #this model/data belongs to (has close relationship with) post model/data takes a post_id
+  #@body
+  #@post_id
+  belongs_to :post    #ARMethod-this model/data belongs to (has close relationship with) post model/data takes a post_id
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
-class Post < ApplicationRecord #ApplicationRecord inherits from ActiveRecord::Base, which defines a number of helpful methods.
+class Post < ApplicationRecord #ApplicationRecord inherits from ActiveRecord::Base, which defines a number of helpful methods for interaction with database.
+    #@title
+    #@body
+    has_many :comments #ARMethod-allows post object to have many comment onbjects related to it + creates some cool methods for us, like (attr_accessor :attribute) dynamically creates getter and setter methods
 end
-
-
-# Active Record helps with the relationship between model and database (ORM)
