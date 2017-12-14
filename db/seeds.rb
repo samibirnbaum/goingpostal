974504c4896d5dee61fedfa4439e6c4da8a06ae0
SAMI BIRNBAUM <sami_birnbaum@btconnect.com>
2017-12-14 14:46:12 +0000

    C16

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "random_data" #requires a separate file of ruby code - often a class
+
+50.times do
+    Post.create!(
+        title: RandomData.random_sentence, #use these methods on the class to create strings for our attributes
+        body: RandomData.random_paragraph
+    )
+end
+
+posts = Post.all #retrieves every post object from the db and stores it in variable called posts - returns array
+
+100.times do
+    Comment.create!(
+        post: posts.sample, #array method to pick out random element, in this case a post object
+        body: RandomData.random_paragraph
+    )
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
