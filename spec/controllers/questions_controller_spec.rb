e06a08fcd8d122c369f47032d9804dfa559ffef5
SAMI BIRNBAUM <sami_birnbaum@btconnect.com>
2017-12-25 14:20:32 +0000

    question show resource complete

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
-
+let(:my_question) {Question.create!(title: "question title", body: "question body", resolved: false)}
   describe "GET #index" do
     it "returns http success" do
       get :index
@@ -14,20 +14,39 @@ RSpec.describe QuestionsController, type: :controller do
     end
 
     it "assigns all the questions to @questions" do
-      Question.create!(title: "question title", body: "question body", resolved: false)
+      Question.create!(title: "question title1", body: "question body1", resolved: false)
       Question.create!(title: "question title2", body: "question body2", resolved: false)
       get :index
       expect(assigns(:questions)).to eq(Question.all)
     end
   end
 
+  
+  
+  
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, params: {id: my_question.id}
       expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #show view" do
+      get :show, params: {id: my_question.id}
+      expect(response).to render_template(:show)
+    end
+
+    it "assigns to @question the question with the id used in the url" do
+      get :show, params: {id: my_question.id}
+      expect(assigns(:question)).to eq(my_question)
     end
   end
 
+  
+  
+  
+  
+  
+  
   describe "GET #new" do
     it "returns http success" do
       get :new
