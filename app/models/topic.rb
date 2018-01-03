b1a978251002cd9e2513558ca036900e27c3dbbc
SAMI BIRNBAUM <sami_birnbaum@btconnect.com>
2018-01-03 13:05:58 +0000

    topic validations set up backend

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,4 +3,11 @@ class Topic < ApplicationRecord
     #@description
     #@public
     has_many :posts, dependent: :destroy #when call destroy on topic also call it on all its posts
+
+    validates_presence_of :name
+    validates_presence_of :description
+    validates_presence_of :public
+
+    validates_length_of :name, minimum: 5
+    validates_length_of :description, minimum: 15
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -8,6 +8,13 @@ RSpec.describe Topic, type: :model do
 
   it { is_expected.to have_many(:posts) }
 
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:public) }
+
+  it { should validate_length_of(:name).is_at_least(5) }
+  it { should validate_length_of(:description).is_at_least(15) }
+
   describe "attributes of the model" do
     it "has all the attributes we generated it with" do
       expect(topic).to have_attributes(name: name, description: description, public: public)
